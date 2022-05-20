5020cffbbef74836b6e19ebd746d8fc2861914db
Zixin <zchong@paypal.com>
2022-05-20 21:07:38 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Uses `face-api.js` to detect your face on browser using webcam. It also runs a simple algorithm to determine if your mouth is open. Credits from [https://towardsdatascience.com/how-to-detect-mouth-open-for-face-login-84ca834dff3b](https://towardsdatascience.com/how-to-detect-mouth-open-for-face-login-84ca834dff3b). I merely converted the algorithm from Python to Javascript.
+
+# Instructions
+
+1. Run `npx http-server`
+2. Visit `http://localhost:8080` on your browser
---
+++ script.js
@@ -0,0 +1,84 @@
+const video = document.getElementById('video')
+const label = document.getElementById('label')
+
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models')
+]).then(startVideo)
+
+function startVideo() { 
+    navigator.mediaDevices.getUserMedia(
+        { video: {} })
+        .then(stream => { 
+            const video = document.getElementById('video') 
+            video.srcObject = stream 
+        })
+        .catch( err => { 
+            console.log('Failed to get local stream' ,err); 
+        }
+    )
+}
+
+startVideo();
+
+video.addEventListener('play', () => {
+    //create the canvas from video element as we have created above
+    const canvas = faceapi.createCanvasFromMedia(video);
+    //append canvas to body or the dom element where you want to append it
+    document.body.append(canvas)
+    // displaySize will help us to match the dimension with video screen and accordingly it will draw our detections
+    // on the streaming video screen
+    const displaySize = { width: video.width, height: video.height }
+    faceapi.matchDimensions(canvas, displaySize)
+    setInterval(async () => {
+      const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+      const resizedDetections = faceapi.resizeResults(detections, displaySize)
+      const context = canvas.getContext("2d")
+      context.clearRect(0, 0, canvas.width, canvas.height)
+      faceapi.draw.drawDetections(canvas, resizedDetections)
+      faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+      faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+
+      let isMouthOpen = checkMouthOpen(detections[0].landmarks.getMouth()) ? "Mouth is open" : "Mouth is closed"
+      context.fillStyle = "white"
+      context.font = "bold 50px Arial"
+      context.fillText(isMouthOpen, (canvas.width / 2) - 200, (canvas.height / 4))
+    }, 100)
+})
+
+function getLipHeight(lip, isUpper) {
+    const indexArray = isUpper ? [[10,19], [9,18], [8,17]] : [[2,13], [3,14], [4,15]]
+    sum = 0
+    for(let [d1,d2] of indexArray) {
+        distance = Math.sqrt((lip[d1].x - lip[d2].x)**2 + 
+                            (lip[d1].y - lip[d2].y)**2)
+        sum += distance
+    }
+    return sum/3
+}
+
+function getMouthHeight(lip) {
+    const indexArray = [[17,15],[18,14],[19,13]]
+    sum = 0
+    for(let [d1,d2] of indexArray) {
+        distance = Math.sqrt((lip[d1].x - lip[d2].x)**2 + 
+                            (lip[d1].y - lip[d2].y)**2)
+        sum += distance
+    }
+    return sum/3
+}
+
+function checkMouthOpen(lip) {
+    topLipHeight = getLipHeight(lip, true)
+    bottomLipHeight = getLipHeight(lip, false)
+    mouthHeight = getMouthHeight(lip)
+    // console.log("topLipHeight: " + topLipHeight)
+    // console.log("bottomLipHeight: " + bottomLipHeight)
+    // console.log("mouthHeight: " + mouthHeight)
+
+    // if mouth is open more than lip height * ratio, return true
+    ratio = 0.5
+    return (mouthHeight > Math.min(topLipHeight, bottomLipHeight) * ratio) ? true : false
+}
